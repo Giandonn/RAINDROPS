3e5bb80bb123658263f9578ce0f45a6c6a1c3506
Lucas Raineri Giandon <88562200+Giandonn@users.noreply.github.com>
2023-03-18 10:38:49 -0300

    Create raindrops.py

---
+++ raindrops.py
@@ -0,0 +1,9 @@
+import webbrowser as wb
+import random as rd
+while True:
+        x = int(input("digite 1 para flutuar:"))
+        if x == 1:
+            wb.open("https://www.youtube.com/watch?v=2WqI50mc7Jo")
+            break
+        else:
+            print("tente novamente!")
