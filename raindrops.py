9379aa1e173e3a61bebc7c0b284820132a461e5c
Lucas Raineri Giandon <88562200+Giandonn@users.noreply.github.com>
2023-03-18 10:56:15 -0300

    Update raindrops.py

--- raindrops.py
+++ raindrops.py
@@ -1,9 +1,15 @@
 import webbrowser as wb
 import random as rd
 while True:
-        x = int(input("digite 1 para flutuar:"))
+        x = int(input("escolha entre 1 e 4 para flutuar:"))
         if x == 1:
             wb.open("https://www.youtube.com/watch?v=2WqI50mc7Jo")
+        elif x == 2:
+            wb.open("https://www.youtube.com/watch?v=2agCnyknvJA&list=RD2agCnyknvJA&start_radio=1")
+        elif x == 3:
+            wb.open("https://www.youtube.com/watch?v=MHryuYVyHhk&list=RD2agCnyknvJA&index=6")
+        elif x == 4:
+            wb.open("https://www.youtube.com/watch?v=_WCD3Z9UmJ4&list=RD2agCnyknvJA&index=9")
             break
         else:
             print("tente novamente!")
